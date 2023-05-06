f553577280ed0ecb4680851c0505deab484ca859
CrystalWD <egixx@mail.ru>
2023-05-06 15:17:51 +0300

    myInit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanHello2 =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(beanHello2.getMessage());
+
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(catBean1.getName()+"\n"+catBean2.getName());
+        System.out.println(bean==beanHello2);
+        System.out.println(catBean1==catBean2);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,11 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat(){
+        Cat newCat = new Cat();
+        newCat.setName("Новый котик");
+        return newCat;
+    }
 }
